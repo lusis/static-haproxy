32f12251e6820309c08cdd75a41a248ff15825cf
John E Vincent <lusis.org+github.com@gmail.com>
2019-03-25 22:55:38 -0400

    add some yum packages

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+yum install -y wget bzip2 patch gcc gcc-c++
 HAPROXY_MAJOR_VERSION=1.9 HAPROXY_MINOR_VERSION=5 PCRE_VERSION=8.43 OPENSSL_VERSION=1.0.2r ZLIB_VERSION=1.2.11 ./compile.sh 
